8eaca68d81dd1651a62ccff5272da8c9fe6a5d10
Deep Patel <pateldeep120404@gmail.com>
2025-03-19 11:57:03 +0530

    push

--- General/src/Components/ClearLocalStorage.jsx
+++ General/src/Components/ClearLocalStorage.jsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
+import { CloudCog } from "lucide-react";
 
 const CheckAuth = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
     const token = Cookies.get("token"); // Get token from cookies
+    console.log(token);
 
     if (!token) {
       // If token is missing in cookies, clear localStorage
