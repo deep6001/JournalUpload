512e229023c84a1f0d65732de750c2ecf639582c
Deep Patel <pateldeep120404@gmail.com>
2025-03-19 11:50:55 +0530

    push

--- General/src/Components/ClearLocalStorage.jsx
+++ General/src/Components/ClearLocalStorage.jsx
@@ -8,17 +8,11 @@ const CheckAuth = () => {
   useEffect(() => {
     const token = Cookies.get("token"); // Get token from cookies
 
-    if (token) {
-      const decodedToken = JSON.parse(atob(token.split(".")[1])); // Decode JWT payload
-      const currentTime = Math.floor(Date.now() / 1000);
-
-      if (decodedToken.exp < currentTime) {
-        localStorage.clear();
-        Cookies.remove("token"); // Remove token from cookies
-        navigate("/login"); // Redirect user to login
-      }
+    if (!token) {
+      // If token is missing in cookies, clear localStorage
+      localStorage.clear();
+      navigate("/login"); // Redirect user to login
     }
-   
   }, [navigate]);
 
   return null;
