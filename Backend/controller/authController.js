244cf80a4512663790187f635a2f6b834bd7a9f8
Deep Patel <pateldeep120404@gmail.com>
2025-03-19 14:32:21 +0530

    push

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -41,7 +41,7 @@ export const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: "None",
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
     
